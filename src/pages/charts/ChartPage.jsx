652cfb139c839f67e4c6f6820788c69fbe6fa0bc
AntoninaSolodskaya <mailstonya@gmail.com>
2019-05-30 22:04:30 +0300

    chart page loading

--- src/pages/charts/ChartPage.jsx
+++ src/pages/charts/ChartPage.jsx
@@ -10,47 +10,49 @@ class ChartPage extends Component {
     series: [{ data: [] }],
     isLoading: false,
     tickets: [],
-    halls: []
+    halls: [],
+    isDataReady: false
   };
 
   getHallTitle = () => {
     api.getHalls()
-    .then(result => {
-      this.setState({
-        options: {
-          chart: {
-            id: result.halls.map(hall => hall._id)
-          },
-          xaxis: {
-            categories: result.halls.map(hall => hall.title)
-          },
-          fill: {
-            colors: ['#F44336']
-          },
-        },
-          isLoading: false, 
-          halls: result.halls
-        });  
-      console.log("categories", this.state.options)
-      console.log("halls",this.state.halls)
-    });
-  };
-
-  getTicketData = () => {
-    api.getTickets()
       .then(result => {
-        let hallsCounter = [0, 0, 0, 0]
-
-        for (let j = 0; j < this.state.halls.length; j++) {
-          hallsCounter[j] = 0
-          for (let i = 0; i < result.length; i++) {
-            if (result[i].hall_id === this.state.halls[j]._id) {
-              hallsCounter[j]++
-              console.log (`hall ${j} `, hallsCounter[j])
+        this.setState({
+          options: {
+            chart: {
+              id: result.halls.map(hall => hall._id)
+            },
+            xaxis: {
+              categories: result.halls.map(hall => hall.title)
             }
-          } 
-          console.log('hall tickets number', hallsCounter[j])
-        }
+          },
+            halls: result.halls,
+            isLoading: true
+          });  
+        console.log("categories", this.state.options)
+        console.log("halls",this.state.halls)
+      })
+      .then(() => {
+        return api.getTickets()
+      })
+      .then(tickets => {
+        let hallsCounter = [];
+
+        this.state.halls.forEach((hall, i) => {
+          hallsCounter[i] = tickets.filter((ticket) => ticket.hall_id === hall._id).length;
+        });
+
+
+//         // for (let j = 0; j < this.state.halls.length; j++) {
+//         //   //hallsCounter[j] = 0
+//         //   for (let i = 0; i < tickets.length; i++) {
+//         //     if (tickets[i].hall_id === this.state.halls[j]._id) {
+//         //       hallsCounter[j]++
+//         //       console.log (`hall ${j} `, hallsCounter[j])
+//         //     }
+//         //   } 
+//         //   console.log('hall tickets number', hallsCounter[j])
+//         // }
 
         console.log('COUNTER', hallsCounter)
 
@@ -59,16 +61,18 @@ class ChartPage extends Component {
             data: hallsCounter
           }],
           isLoading: true, 
-          tickets: result
+          tickets: tickets,
+          isDataReady: true
         }); 
         console.log('series', this.state.series)
         console.log("tickets", this.state.tickets.length)
-      })
+      });;
   };
+
+  
 
   componentDidMount() {
     this.getHallTitle();
-    this.getTicketData();
   };
 
   render() {
@@ -76,13 +80,15 @@ class ChartPage extends Component {
       <Wrap>
         <Block>
           <div className="mixed-chart">
-            <Chart 
+            {this.state.isDataReady && (
+              <Chart 
               options={this.state.options}
               series={this.state.series}
               type="bar"
               max-width="900px"
               min-width="300px"
             /> 
+            )}  
           </div>
         </Block>
       </Wrap>
